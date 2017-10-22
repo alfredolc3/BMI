b3021ad563e1c6c37e27a46de643cfc7bcc57dde
Roger Burgos <burgosr90@gmail.com>
2017-10-22 15:06:02 -0500

    Google maps

--- public/js/predios/especificos/especificos.js
+++ public/js/predios/especificos/especificos.js
@@ -1,36 +1,75 @@
-var cp=$("#cp");
-var cpbuscar=$("#cp-buscar");
+var cp = $("#cp");
+var cpbuscar = $("#cp-buscar");
+var marcadores = [];
+var latitud = $("#latitud");
+var longitud = $("#longitud");
+var altitud = $("#altitud");
 
-$(document).on("ready", function(){
+$(document).on("ready", function () {
 
-	cpbuscar.on("click", function(event){
-			buscarcp();
-	});
+    cpbuscar.on("click", function (event) {
+        buscarcp();
+    });
 });
 
-function buscarcp(){
-	var divAsentamientos=$("#asentamientos");
-	var urlbuscar = '/datos/buscar-cp';
-	var datosbuscar = {
-		cp: cp.val()
-	};
-	var jsonbuscar = funciones.forms(urlbuscar, 'GET', datosbuscar);
+function initMap() {
+    var myLatlng = {lat: 23.8295505, lng: -102.5732324};
 
-	divAsentamientos.empty();
-	if(jsonbuscar.bandera){
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
+        center: myLatlng
+    });
+    var elevator = new google.maps.ElevationService;
 
-		var divColMd=$('<div class="col-md-4">');
-		var divFormGroup=$('<div class="form-group">');
-		var labelColonia=$('<label>Colonia</label>');
-		var selectColonia=$('<select class="form-control" name="idSepomex"/>');
+    map.addListener('click', function (e) {
 
-		$.each(jsonbuscar.data, function(k, v){
-			selectColonia.append('<option value="'+v.id+'">'+v.asentamiento+'</option>');
-		});
+        for (var i = 0; i < marcadores.length; i++) {
+            marcadores[i].setMap(null);
+        }
+        marcadores = [];
 
-		divFormGroup.append(labelColonia).append(selectColonia);
-		divColMd.append(divFormGroup);
-		divAsentamientos.append(divColMd);
+        marcador = new google.maps.Marker({
+            position: e.latLng,
+            map: map
+        });
+        map.panTo(e.latLng);
+        marcadores[0] = marcador;
 
-	}
+        latitud.val(e.latLng.lat());
+        longitud.val(e.latLng.lng());
+
+        elevator.getElevationForLocations({
+            'locations': [e.latLng]
+        }, function (results, status) {
+            altitud.val(results[0].elevation);
+        });
+
+    });
+}
+
+function buscarcp() {
+    var divAsentamientos = $("#asentamientos");
+    var urlbuscar = '/datos/buscar-cp';
+    var datosbuscar = {
+        cp: cp.val()
+    };
+    var jsonbuscar = funciones.forms(urlbuscar, 'GET', datosbuscar);
+
+    divAsentamientos.empty();
+    if (jsonbuscar.bandera) {
+
+        var divColMd = $('<div class="col-md-4">');
+        var divFormGroup = $('<div class="form-group">');
+        var labelColonia = $('<label>Colonia</label>');
+        var selectColonia = $('<select class="form-control" name="idSepomex"/>');
+
+        $.each(jsonbuscar.data, function (k, v) {
+            selectColonia.append('<option value="' + v.id + '">' + v.asentamiento + '</option>');
+        });
+
+        divFormGroup.append(labelColonia).append(selectColonia);
+        divColMd.append(divFormGroup);
+        divAsentamientos.append(divColMd);
+
+    }
 }
--- resources/views/predios/especificos/index.blade.php
+++ resources/views/predios/especificos/index.blade.php
@@ -1,162 +1,178 @@
 @extends('app')
 
 @section('css')
-<style type="text/css">
-	#map {
-        height: 100%;
-      }
-</style>
+    <style type="text/css">
+        #map {
+            height: 100%;
+        }
+    </style>
 @endsection
 
 @section('htmlheader_title')
-Nuevo Predio
+    Nuevo Predio
 @endsection
 
 
 @section('main-content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Datos Especificos</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Datos Especificos</div>
 
-				<div class="panel-body">
-					@include('partials.error')
-					{!!Form::open(['route'=>'datos.especificos.store', 'method'=>'POST'])!!}
-					
-					{!! Form::hidden('idDatosPrincipales', $idDatosPrincipales) !!}
+                    <div class="panel-body">
+                        @include('partials.error')
+                        {!!Form::open(['route'=>'datos.especificos.store', 'method'=>'POST'])!!}
 
-					<div class="row">
-						<div class="col-md-10">
-					<div class="d-inline-block bg-primary"><h3><CENTER><B>UBICACION DEL PREDIO</B></CENTER></h3></div>
-				</div>
-						<div class="col-md-4">
-							<div class="form-group">
-								{!!Form::label('calle', 'Calle')!!}
-								{!!Form::text('calle', null, ['class'=>'form-control'])!!}
-							</div>
-						</div>
-						
-						<div class="col-md-1">
-							<div class="form-group">
-								{!!Form::label('numeroInt.', 'N. Int')!!}
-								{!!Form::text('numeroInt.', null, ['class'=>'form-control'])!!}
-							</div>
-						</div>
+                        {!! Form::hidden('idDatosPrincipales', $idDatosPrincipales) !!}
 
-						<div class="col-md-1">
-							<div class="form-group">
-								{!!Form::label('numeroExt.', 'N. Ext')!!}
-								{!!Form::text('numeroExt.', null, ['class'=>'form-control'])!!}
-							</div>
-						</div>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="d-inline-block bg-primary">
+                                    <h3>
+                                        <CENTER><B>UBICACION DEL PREDIO</B></CENTER>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {!!Form::label('calle', 'Calle')!!}
+                                    {!!Form::text('calle', null, ['class'=>'form-control'])!!}
+                                </div>
+                            </div>
 
-						<div class="col-md-2">
-							<div class="form-group">
-								{!!Form::label('cp', 'Codigo Postal')!!}
-								<div class="input-group">
-									{!!Form::number('cp', null,['class'=>'form-control'])!!}
-									<span class="input-group-btn">
+                            <div class="col-md-1">
+                                <div class="form-group">
+                                    {!!Form::label('numeroInt.', 'N. Int')!!}
+                                    {!!Form::text('numeroInt.', null, ['class'=>'form-control'])!!}
+                                </div>
+                            </div>
+
+                            <div class="col-md-1">
+                                <div class="form-group">
+                                    {!!Form::label('numeroExt.', 'N. Ext')!!}
+                                    {!!Form::text('numeroExt.', null, ['class'=>'form-control'])!!}
+                                </div>
+                            </div>
+
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    {!!Form::label('cp', 'Codigo Postal')!!}
+                                    <div class="input-group">
+                                        {!!Form::number('cp', null,['class'=>'form-control'])!!}
+                                        <span class="input-group-btn">
 										<button class="btn btn-warning" id="cp-buscar" type="button" data-toggle="tooltip" title="Buscar"><i class="fa fa-search"></i></button>
 									</span>
-								</div><!-- /input-group -->
-							</div>
-						</div>
-						<div id="asentamientos"></div>
-						
-					</div>
+                                    </div><!-- /input-group -->
+                                </div>
+                            </div>
+                            <div id="asentamientos"></div>
+
+                        </div>
 
 
-					<div class="row">
-						<div class="col-md-10">
-							<div class="d-inline-block bg-primary"><h3><CENTER><B>COORDENADAS GEOGRAFICAS</B></CENTER></h3></div>
-						</div>
-						<div id="map"></div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('longitud', 'Longitud')!!}
-								{!!Form::number('longitud', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Longitud'])!!}
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('latitud', 'Latitud')!!}
-								{!!Form::number('latitud', null,['class'=>'form-control', 'step'=>'any','placeholder'=>'Latitud'])!!}
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('altitud', 'Altitud')!!}
-								{!!Form::number('altitud', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Altitud'])!!}
-							</div>			
-						</div>															
-					</div>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="d-inline-block bg-primary">
+                                    <h3>
+                                        <CENTER><B>COORDENADAS GEOGRAFICAS</B></CENTER>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div id="map" style="margin: 0 auto; width:90%; height:400px;"></div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('longitud', 'Longitud')!!}
+                                    {!!Form::number('longitud', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Longitud'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('latitud', 'Latitud')!!}
+                                    {!!Form::number('latitud', null,['class'=>'form-control', 'step'=>'any','placeholder'=>'Latitud'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('altitud', 'Altitud')!!}
+                                    {!!Form::number('altitud', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Altitud'])!!}
+                                </div>
+                            </div>
+                        </div>
 
-					<div class="row">
-						<div class="col-md-10">
-						<div class="d-inline-block bg-primary"><h3><CENTER><B>TIPO Y REGIMEN </B></CENTER></h3></div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('tipoPredio', 'Tipo de Predio')!!}
-								{!!Form::select('tipoPredio', ['Urbano'=>'Urbano', 'Rustico'=>'Rustico'], null, ['class'=>'form-control', 'placeholder' => 'Seleccione tipo de inmueble'])!!}
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('idRegimenPropiedad', 'Regimen de Propiedad')!!}
-								{!!Form::select('idRegimenPropiedad', $regimen, null, ['class'=>'form-control', 'placeholder' => 'Seleccione Regimen ....'])!!}
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('idTipoTerreno', 'Tipo de Terreno')!!}
-								{!!Form::select('idTipoTerreno', $tipoTerreno, null, ['class'=>'form-control', 'placeholder' => 'Seleccione Tipo de Terreno...'])!!}
-							</div>
-						</div>
-					</div>
-
-					
-					<div class="row">
-						<div class="col-md-10">
-							<div class="d-inline-block bg-primary"><h3><CENTER><B>SUPERFICIES</B></CENTER></h3></div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('superficieTerreno', 'Superficie de Terreno')!!}
-								{!!Form::number('superficieTerreno', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Superficie de Terreno'])!!}
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								{!!Form::label('superficieConstruccion', 'Superficie de Construccion')!!}
-								{!!Form::number('superficieConstruccion', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Superficie de Construccion'])!!}
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="form-group">
-								{!!Form::label('idUsoSuelo', 'Uso de Suelo')!!}
-								{!!Form::select('idUsoSuelo', $usoSuelo, null, ['class'=>'form-control', 'placeholder' => 'Seleccione uso de suelo...'])!!}
-							</div>
-						</div>
-						
-
-					</div>
-					<div class="form-group">
-						{!! Form::submit('Registrar',['class' => 'btn btn-primary']) !!}
-					</div>	
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="d-inline-block bg-primary">
+                                    <h3>
+                                        <CENTER><B>TIPO Y REGIMEN </B></CENTER>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('tipoPredio', 'Tipo de Predio')!!}
+                                    {!!Form::select('tipoPredio', ['Urbano'=>'Urbano', 'Rustico'=>'Rustico'], null, ['class'=>'form-control', 'placeholder' => 'Seleccione tipo de inmueble'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('idRegimenPropiedad', 'Regimen de Propiedad')!!}
+                                    {!!Form::select('idRegimenPropiedad', $regimen, null, ['class'=>'form-control', 'placeholder' => 'Seleccione Regimen ....'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('idTipoTerreno', 'Tipo de Terreno')!!}
+                                    {!!Form::select('idTipoTerreno', $tipoTerreno, null, ['class'=>'form-control', 'placeholder' => 'Seleccione Tipo de Terreno...'])!!}
+                                </div>
+                            </div>
+                        </div>
 
 
-					{!!Form::close()!!}	
-					
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="d-inline-block bg-primary">
+                                    <h3>
+                                        <CENTER><B>SUPERFICIES</B></CENTER>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('superficieTerreno', 'Superficie de Terreno')!!}
+                                    {!!Form::number('superficieTerreno', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Superficie de Terreno'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    {!!Form::label('superficieConstruccion', 'Superficie de Construccion')!!}
+                                    {!!Form::number('superficieConstruccion', null,['class'=>'form-control', 'step'=>'any', 'placeholder'=>'Superficie de Construccion'])!!}
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {!!Form::label('idUsoSuelo', 'Uso de Suelo')!!}
+                                    {!!Form::select('idUsoSuelo', $usoSuelo, null, ['class'=>'form-control', 'placeholder' => 'Seleccione uso de suelo...'])!!}
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit('Registrar',['class' => 'btn btn-primary']) !!}
+                        </div>
+
+
+                        {!!Form::close()!!}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJ0i7nvxdKfnGnPzXmk7AHZCbBUopZr-4"></script>
-<script type="text/javascript" src="/js/predios/especificos/especificos.js"></script>
+    <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJ0i7nvxdKfnGnPzXmk7AHZCbBUopZr-4&callback=initMap"></script>
+    <script type="text/javascript" src="/js/predios/especificos/especificos.js"></script>
 @endsection
