67b00d5a36dc2291a16a7390dd203bbc99e1c5ed
alfredo lopez carrasco <alfredolc3@gmail.com>
2017-11-20 10:58:55 -0600

    CONFIG

--- resources/views/partials/htmlheader.blade.php
+++ resources/views/partials/htmlheader.blade.php
@@ -1,5 +1,12 @@
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="cache-control" content="no-store" />
+    <meta http-equiv="cache-control" content="must-revalidate" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
     <title> BMI - @yield('htmlheader_title', 'BASE DE MERCADO INMOBILIARIO') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
